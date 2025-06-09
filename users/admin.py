7ebdf048a3bb4b33d0c9b5de5ca9b028c5ec7638
Getulio Lima <getuliofurtado03@gmail.com>
2025-06-09 13:27:30 -0300

    fix: admin user view

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,9 @@
+# accounts/admin.py
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
 @admin.register(CustomUser)
-class CustomUserAdmin(UserAdmin):
-    fieldsets = UserAdmin.fieldsets + (
-        ("Additional Info", {"fields": ("bio",)}),
-    )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        ("Additional Info", {"fields": ("bio",)}),
-    )
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'favPokemon', 'is_staff')
+    search_fields = ('username', 'email')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
